fde2798267f73730e06c74f6bb9dfdccc87a67c6
Khaliam <mikhailsidelev@gmail.com>
2023-11-01 21:05:28 -0400

    i have added the planet2

--- src/test/java/SolarSystem/main.java
+++ src/test/java/SolarSystem/main.java
@@ -8,3 +8,10 @@ public class main {
     solarsys.Stars = "2";
     solarsys.Planets = "10";
 }
+    //        **planet2**
+    Planet2 xaros=new Planet2();
+        xaros.name="nehaio";
+                xaros.color="blue";
+                xaros.size="32432432324324km";
+                }
+                }
